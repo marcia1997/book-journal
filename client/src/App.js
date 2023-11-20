8ea19e85ffb363f9334455425d8b159ffae4cf34
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2023-11-20 19:56:53 +0200

    app creation

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home.jsx';
+import Login from './pages/Login/Login.jsx';
+import Register from './pages/Register/Register.jsx';
+import SingleBook from './pages/SingleBook/SingleBook.jsx';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/book" element={<SingleBook/>} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
