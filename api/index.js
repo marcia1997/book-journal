b8f09845b652972b1e9ebb3206261e62181d6749
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2023-11-23 12:55:31 +0200

    Index.js creation

---
+++ api/index.js
@@ -0,0 +1,31 @@
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// MongoDB connection
+mongoose.connect('mongodb+srv://marciadenisevazquez:marciadenisevazquez@cluster-book-app.yy6jcgi.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
+});
+
+//routes
+const authRouter = require('./routes/auth');
+const booksRouter = require('./routes/books');
+
+app.use('/auth', authRouter);
+app.use('/books', booksRouter);
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
