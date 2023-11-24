2fc4a3ca801518a9dc45891b406e39a94adf8377
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2023-11-24 15:04:50 +0200

    Routes Auth

---
+++ api/routes/auth.js
@@ -0,0 +1,51 @@
+const router = require("express").Router();
+const User = require("../models/User.js");
+const bcrypt = require("bcrypt");
+//REGISTER
+router.post("/register", async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(req.body.password, salt);
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPass,
+    });
+
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// LOGIN
+router.post("/login", async (req, res) => {
+    try {
+      const user = await User.findOne({ username: req.body.username });
+  
+      // Check if the user exists
+      if (!user) {
+        return res.status(400).json("Wrong credentials!");
+      }
+  
+      // Compare passwords
+      const validated = await bcrypt.compare(req.body.password, user.password);
+  
+      // Check if passwords match
+      if (!validated) {
+        return res.status(400).json("Wrong credentials!");
+      }
+  
+      // If credentials are correct, send a response with user details (excluding password)
+      const { password, ...others } = user._doc;
+      res.status(200).json(others);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json("Internal Server Error");
+    }
+  });
+  
+  module.exports = router;
+
+module.exports = router;
