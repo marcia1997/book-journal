a32cc6fbdc611fade04b45286494e3348f5fbac0
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2023-12-12 00:59:20 +0200

    update auth

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,51 +1,63 @@
-const router = require("express").Router();
-const User = require("../models/User.js");
-const bcrypt = require("bcrypt");
-//REGISTER
-router.post("/register", async (req, res) => {
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+
+// Handle user registration
+router.post('/register', async (req, res) => {
   try {
-    const salt = await bcrypt.genSalt(10);
-    const hashedPass = await bcrypt.hash(req.body.password, salt);
+    const { username, email, password } = req.body;
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user with the hashed password
     const newUser = new User({
-      username: req.body.username,
-      email: req.body.email,
-      password: hashedPass,
+      username,
+      email,
+      password: hashedPassword,
     });
 
-    const user = await newUser.save();
-    res.status(200).json(user);
-  } catch (err) {
-    res.status(500).json(err);
+    // Save the user to the database
+    const savedUser = await newUser.save();
+
+    // Respond with a customized user object (excluding the password)
+    const { password: _, ...userWithoutPassword } = savedUser._doc;
+    res.json(userWithoutPassword);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
-// LOGIN
-router.post("/login", async (req, res) => {
-    try {
-      const user = await User.findOne({ username: req.body.username });
-  
-      // Check if the user exists
-      if (!user) {
-        return res.status(400).json("Wrong credentials!");
-      }
-  
-      // Compare passwords
-      const validated = await bcrypt.compare(req.body.password, user.password);
-  
-      // Check if passwords match
-      if (!validated) {
-        return res.status(400).json("Wrong credentials!");
-      }
-  
-      // If credentials are correct, send a response with user details (excluding password)
-      const { password, ...others } = user._doc;
-      res.status(200).json(others);
-    } catch (err) {
-      console.error(err);
-      res.status(500).json("Internal Server Error");
+// LOGIN - With Password Hashing
+router.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Find the user in the database by username
+    const user = await User.findOne({ username });
+
+    // Check if the user exists
+    if (!user) {
+      return res.status(400).json("Wrong credentials!");
     }
-  });
-  
-  module.exports = router;
+
+    // Compare the hashed password
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    // Check if the passwords match
+    if (!passwordMatch) {
+      return res.status(400).json("Wrong credentials!");
+    }
+
+    // Passwords match, send user data excluding the password
+    const { password: _, ...userWithoutPassword } = user._doc;
+    res.status(200).json(userWithoutPassword);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json("Internal Server Error");
+  }
+});
 
 module.exports = router;
