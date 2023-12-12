0a8e38687897905783e05e52fff3764303cbf8e0
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2023-12-12 17:31:42 +0200

    create singlebook

---
+++ client/src/components/singleBook.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const BookItem = styled.li`
+  margin-bottom: 20px;
+  list-style: none;
+  width: calc(33.33% - 10px);
+`;
+
+const BookImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 8px; /* Add border-radius for rounded corners */
+`;
+
+const BookTitle = styled.h3`
+  margin-top: 10px;
+  font-size: 1.2rem;
+  color: #333;
+`;
+
+const BookAuthor = styled.p`
+  font-size: 1rem;
+  color: #555;
+`;
+
+const Book = ({ book }) => {
+  const { title, author, imageUrl } = book;
+
+  return (
+    <BookItem>
+      <BookImage src={imageUrl} alt={title} />
+      <BookTitle>{title}</BookTitle>
+      <BookAuthor>{author}</BookAuthor>
+    </BookItem>
+  );
+};
+
+export default Book;
