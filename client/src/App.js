34bd132a0ae8a61063561eeed444ddffcdb80d42
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2023-12-12 12:52:55 +0200

    App update

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,27 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from "./context/AuthContext.js";
 import Home from './pages/Home/Home.jsx';
 import Login from './pages/Login/Login.jsx';
 import Register from './pages/Register/Register.jsx';
 import AddBook from './pages/AddBook/AddBook.jsx';
+import Books from './components/books'
 
 const App = () => {
   return (
+    
     <Router>
-      <Routes>
-        <Route path="/home" element={<Home/>} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/book" element={<AddBook/>} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/book" element={<AddBook />} />
+          <Route path="/books" element={<Books />} />
+        </Routes>
+      </AuthProvider>
     </Router>
   );
 };
-
 export default App;
