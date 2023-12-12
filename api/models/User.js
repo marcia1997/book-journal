34607a606df83e7b23986d709fbdd12e0bf2e0b5
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2023-12-12 17:32:50 +0200

    change on user model

--- api/models/User.js
+++ api/models/User.js
@@ -1,23 +1,29 @@
-const mongoose = require("mongoose");
-const UserSchema = new mongoose.Schema
-({
-    username:{
-        type:String,
-        required:true,
-        unique:true,
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    password:{
-        type:String,
-        required:true
-    },
- },
- //update data
- { timestamps: true }
-);
+// models/user.js
 
-module.exports = mongoose.model("User", UserSchema);
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
