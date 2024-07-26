3265b07c01beb8ed79d2b58aa9f5b7b515580a4f
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2024-07-26 15:32:28 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
 const cors = require('cors');
 const multer = require('multer');
 
@@ -8,8 +9,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(bodyParser.json());
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(fileUpload());
 app.use(cors({
   origin: 'http://localhost:3000', 
   credentials: true,
@@ -20,7 +21,7 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 // MongoDB connection
-mongoose.connect('mongodb+srv://marciadenisevazquez:1234@cluster-book-app.yy6jcgi.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect("mongodb+srv://marciadenisevazquez:1234@cluster-book-app.yy6jcgi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-book-app");
 
 const connection = mongoose.connection;
 connection.once('open', () => {
@@ -32,7 +33,7 @@ const authRouter = require('./routes/auth.js');
 const booksRouter = require('./routes/books.js');
 
 app.use('/auth', authRouter);
-app.use('/api/books', upload.single('coverImage'), booksRouter); 
+app.use('/books', upload.single('coverImage'), booksRouter); 
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
