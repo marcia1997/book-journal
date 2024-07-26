17ad1d2f31f71e2e2ae5865c3f1a666a87de8110
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2024-07-26 15:33:23 +0300

    Update Book.js

--- api/models/Book.js
+++ api/models/Book.js
@@ -1,47 +1,14 @@
-// models/Book.js
-
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  coverImage: {
-    type: Buffer,  // Use Buffer to store binary data
-    required: true,
-  },
-  coverImageType: {
-    type: String,  // Store the MIME type of the image
-    required: true,
-  },
-  startDate: {
-    type: Date,
-    required: true
-  },
-  endDate: {
-    type: Date,
-    required: true
-  },
-  status: {
-    type: String,
-    enum: ['Wish', 'Currently Reading', 'Stop it', 'Read'],
-    required: true
-  },
-  feeling: {
-    type: String,
-    required: true
-  },
-  rating: {
-    type: Number,
-    required: true
-  },
-  review: {
-    type: String,
-    required: true
-  }
+  title: { type: String, required: true },
+  status: { type: String, required: true },
+  feeling: { type: String, required: true },
+  startDate: { type: Date, required: true },
+  endDate: { type: Date, required: true },
+  rating: { type: Number, required: true },
+  review: { type: String, required: true },
+  coverImage: { type: Buffer, contentType: String }, // Adjust as needed
 });
 
-const Book = mongoose.model('Book', bookSchema);
-
-module.exports = Book;
+module.exports = mongoose.model('Book', bookSchema);
